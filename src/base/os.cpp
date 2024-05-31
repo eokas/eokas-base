bb0a6230949450436b9409d23644d08fe9780779
Qilin <qilin.cheng@foxmail.com>
2024-05-31 09:21:07 +0800

    OS APIs

--- src/base/os.cpp
+++ src/base/os.cpp
@@ -1,20 +1,17 @@
 
 #include "./os.h"
+#include <cstdlib>
 
 #if _EOKAS_OS == _EOKAS_OS_WIN64 || _EOKAS_OS == _EOKAS_OS_WIN32
-
 #include <Windows.h>
 #include <comdef.h>
 #include <Wbemidl.h>
 #include <iphlpapi.h>
 #pragma comment(lib, "wbemuuid.lib")
-
 #else
-
 #include <limits.h>
 #include <unistd.h>
 #include <sys/utsname.h>
-
 #endif
 
 namespace eokas {
@@ -46,373 +43,97 @@ namespace eokas {
         if (!GetComputerNameA(buffer, &count))
             return "";
         return String(buffer, count);
-#elif _EOKAS_OS == _EOKAS_OS_LINUX || _EOKAS_OS == _EOKAS_OS_MACOS
+#else
         char buffer[HOST_NAME_MAX] = {0};
         gethostname(buffer, HOST_NAME_MAX);
         return String(buffer);
-#else
-
 #endif
     }
     
     String OS::getDeviceModel() {
-    
-    }
-}
-
-#if _EOKAS_OS == _EOKAS_OS_WIN64 || _EOKAS_OS == _EOKAS_OS_WIN32
-
-#include <Windows.h>
-#include <comdef.h>
-#include <Wbemidl.h>
-#include <iphlpapi.h>
-
-#pragma comment(lib, "wbemuuid.lib")
-
-namespace eokas {
-    /**
-     * WMI: Windows Management Infrastructure
-     * https://learn.microsoft.com/en-us/windows/win32/wmisdk/example--getting-wmi-data-from-the-local-computer
-     * */
-    
-    class WMIQuery {
-        _ForbidCopy(WMIQuery);
-        
-        _ForbidAssign(WMIQuery);
-    
-    public:
-        WMIQuery(IEnumWbemClassObject* enumerator)
-            : m_Enumerator(enumerator) {
-            while (m_Enumerator) {
-                ULONG uReturn = 0;
-                HRESULT hr = m_Enumerator->Next(WBEM_INFINITE, 1, &m_ClassObject, &uReturn);
-                if (uReturn == 0) {
-                    break;
-                }
-            }
-            
-        }
-        
-        virtual ~WMIQuery() {
-            if (m_ClassObject != NULL) {
-                m_ClassObject->Release();
-                m_ClassObject = NULL;
-            }
-            if (m_Enumerator != NULL) {
-                m_Enumerator->Release();
-                m_Enumerator = NULL;
-            }
-        }
-        
-        std::string getValue(const std::string& key) {
-            if (m_ClassObject == NULL)
-                return "";
-            
-            std::wstring propKey = String::utf8ToUnicode(key, false);
-            
-            VARIANT propVal;
-            VariantInit(&propVal);
-            
-            // Get the value of the Name property
-            HRESULT hr = m_ClassObject->Get(propKey.c_str(), 0, &propVal, 0, 0);
-            if (FAILED(hr)) {
-                VariantClear(&propVal);
-                return "";
-            }
-            
-            std::string ret = String::unicodeToUtf8(propVal.bstrVal, false);
-            
-            VariantClear(&propVal);
-            
-            return ret;
-        }
-    
-    private:
-        IEnumWbemClassObject* m_Enumerator = NULL;
-        IWbemClassObject* m_ClassObject = NULL;
-    };
-    
-    class WMIScope {
-        _ForbidCopy(WMIScope);
-        
-        _ForbidAssign(WMIScope);
-    
-    public:
-        static WMIScope& local() {
-            static const wchar_t* server = L"ROOT\\CIMV2";
-            static WMIScope scope(server);
-            return scope;
-        }
-    
-    public:
-        explicit WMIScope(const wchar_t* server) {
-            this->init(server);
-        }
-        
-        ~WMIScope() {
-            this->quit();
-        }
-        
-        bool init(const wchar_t* server) {
-            /**
-             * https://learn.microsoft.com/en-us/windows/win32/api/combaseapi/nf-combaseapi-coinitializeex
-             * */
-            HRESULT hr = CoInitializeEx(NULL, COINIT_MULTITHREADED);
-            if (FAILED(hr)) {
-                return false;
-            }
-            
-            hr = CoInitializeSecurity(NULL, -1,                             // COM authentication
-                NULL,                           // Authentication services
-                NULL,                           // Reserved
-                RPC_C_AUTHN_LEVEL_DEFAULT,   // Default authentication
-                RPC_C_IMP_LEVEL_IMPERSONATE, // Default Impersonation
-                NULL,                        // Authentication info
-                EOAC_NONE,                   // Additional capabilities
-                NULL                         // Reserved
-            );
-            if (FAILED(hr)) {
-                this->quit();
-                return false;
-            }
-            
-            hr = CoCreateInstance(CLSID_WbemLocator, 0, CLSCTX_INPROC_SERVER, IID_IWbemLocator, (LPVOID*) &m_Locator);
-            if (FAILED(hr)) {
-                this->quit();
-                return false;
-            }
-            
-            hr = m_Locator->ConnectServer(_bstr_t(server), // Object path of WMI namespace
-                NULL,                    // User name. NULL = current user
-                NULL,                    // User password. NULL = current
-                0,                       // Locale. NULL indicates current
-                NULL,                    // Security flags.
-                0,                       // Authority (for example, Kerberos)
-                0,                       // Context object
-                &m_Services           // pointer to IWbemServices proxy
-            );
-            if (FAILED(hr)) {
-                this->quit();
-                return false;
-            }
-            
-            hr = CoSetProxyBlanket(m_Services,                        // Indicates the proxy to set
-                RPC_C_AUTHN_WINNT,           // RPC_C_AUTHN_xxx
-                RPC_C_AUTHZ_NONE,            // RPC_C_AUTHZ_xxx
-                NULL,                        // Server principal name
-                RPC_C_AUTHN_LEVEL_CALL,      // RPC_C_AUTHN_LEVEL_xxx
-                RPC_C_IMP_LEVEL_IMPERSONATE, // RPC_C_IMP_LEVEL_xxx
-                NULL,                        // client identity
-                EOAC_NONE                    // proxy capabilities
-            );
-            if (FAILED(hr)) {
-                this->quit();
-                return false;
-            }
-            
-            return true;
-        }
-        
-        void quit() {
-            _DeleteList(m_Queries);
-            
-            if (m_Services != NULL) {
-                m_Services->Release();
-                m_Services = NULL;
-            }
-            if (m_Locator != NULL) {
-                m_Locator->Release();
-                m_Locator = NULL;
-            }
-            
-            CoUninitialize();
-        }
-        
-        WMIQuery* query(const char* wql) {
-            if (m_Locator == NULL || m_Services == NULL)
-                return NULL;
-            
-            IEnumWbemClassObject* enumerator = NULL;
-            HRESULT hr = m_Services->ExecQuery(bstr_t("WQL"), bstr_t(wql), WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY, NULL, &enumerator);
-            if (FAILED(hr)) {
-                return NULL;
-            }
-            
-            WMIQuery* q = new WMIQuery(enumerator);
-            m_Queries.push_back(q);
-            
-            return q;
-        }
-    
-    private:
-        IWbemLocator* m_Locator = NULL;
-        IWbemServices* m_Services = NULL;
-        std::vector<WMIQuery*> m_Queries = {};
-    };
-    
-    struct WindowsInfo {
-        static String getBaseboardSerialNumber() {
-            auto* query = WMIScope::local().query("SELECT * FROM Win32_Baseboard");
-            return query->getValue("SerialNumber");
-        }
-        
-        static String getBiosSerialNumber() {
-            auto* query = WMIScope::local().query("SELECT * FROM Win32_BIOS");
-            return query->getValue("SerialNumber");
-        }
-        
-        static String getProcessorUniqueId() {
-            auto* query = WMIScope::local().query("SELECT * FROM Win32_Processor");
-            return query->getValue("UniqueId");
-        }
-        
-        static String getDiskDriveSerialNumber() {
-            auto* query = WMIScope::local().query("SELECT * FROM Win32_DiskDrive");
-            return query->getValue("SerialNumber");
-        }
-        
-        static String getOperatingSystemSerialNumber() {
-            auto* query = WMIScope::local().query("SELECT * FROM Win32_OperatingSystem");
-            return query->getValue("SerialNumber");
-        }
-    };
-    
-    String OS::getSystemVersion() {
         return "";
     }
     
-    String OS::getDeviceName() {
-    
-    };
-    
     u32_t OS::getCpuCount() {
+#if _EOKAS_OS == _EOKAS_OS_WIN64 || _EOKAS_OS == _EOKAS_OS_WIN32
         SYSTEM_INFO sysInfo;
         GetSystemInfo(&sysInfo);
         u32_t cpuCount = sysInfo.dwNumberOfProcessors;
         return cpuCount;
-    }
-    
-    CpuState& OS::getCpuState(u32_t index) {
-        static CpuState cpu;
-        return cpu;
-    }
-    
-    MemoryState& OS::getMemoryState() {
-        static MemoryState result;
-        if (result.total > 0)
-            return result;
-        
-        MEMORYSTATUSEX memstat;
-        memstat.dwLength = sizeof(memstat);
-        if (!GlobalMemoryStatusEx(&memstat)) {
-            return result;
-        }
-        
-        result.total = memstat.ullTotalPhys;
-        result.available = memstat.ullAvailPhys;
-        return result;
+#else
+        u32_t cpuCount = sysconf(_SC_NPROCESSORS_ONLN);
+        return cpuCount;
+#endif
     }
     
     String OS::getEnv(const String& name) {
-        if (name.isEmpty())
-            return "";
-        
-        char buffer[512] = {0};
-        u32_t length = _ArrayLength(buffer);
-        
-        u32_t len = GetEnvironmentVariableA(name.cstr(), buffer, length);
-        
-        return String(buffer, len);
+        return std::getenv(name.cstr());
     }
     
-    void OS::setEnv(const String& name, const String& value) {
-        if (name.isEmpty())
-            return;
-        SetEnvironmentVariableA(name.cstr(), value.cstr());
+    bool OS::setEnv(const String& name, const String& value) {
+#if _EOKAS_OS == _EOKAS_OS_WIN64 || _EOKAS_OS == _EOKAS_OS_WIN32
+        return _putenv_s(name.cstr(), value.cstr()) == 0;
+#else
+        return setenv(name.cstr(), value.cstr(), 1) == 0;
+#endif
     }
     
     String OS::getCurrentUser() {
+#if _EOKAS_OS == _EOKAS_OS_WIN64 || _EOKAS_OS == _EOKAS_OS_WIN32
         CHAR buffer[256];
         DWORD count = 0;
         if (!GetUserNameA(buffer, &count))
             return "";
         return String(buffer, count);
-    }
-    
-    u32_t OS::getCurrentProcess() {
-        return GetCurrentProcessId();
-    }
-    
-    u32_t OS::getCurrentThread() {
-        return GetCurrentThreadId();
+#else
+        uid_t uid = geteuid();
+        struct passwd* pw = getpwuid(uid);
+        if (pw == nullptr)
+            return "";
+        return pw->pw_name;
+#endif
     }
     
     String OS::getMacAddress() {
-        IP_ADAPTER_INFO AdapterInfo[16];
-        DWORD dwBufLen = sizeof(AdapterInfo);
-        
-        DWORD dwStatus = GetAdaptersInfo(AdapterInfo, &dwBufLen);
-        if (dwStatus != ERROR_SUCCESS) {
+#if _EOKAS_OS == _EOKAS_OS_WIN64 || _EOKAS_OS == _EOKAS_OS_WIN32
+        IP_ADAPTER_INFO adapterInfo[16];
+        DWORD dwBufLen = sizeof(adapterInfo);
+        if (GetAdaptersInfo(adapterInfo, &dwBufLen) != ERROR_SUCCESS)
             return "";
-        }
-        
-        PIP_ADAPTER_INFO pAdapterInfo = AdapterInfo;
-        
-        String macAddress;
-        while (pAdapterInfo) {
-            macAddress += pAdapterInfo->Address[0];
-            for (UINT i = 1; i < pAdapterInfo->AddressLength; i++) {
-                macAddress += "-" + pAdapterInfo->Address[i];
-            }
-            pAdapterInfo = pAdapterInfo->Next;
-        }
-        
-        return macAddress;
-    }
-}
-
-#elif _EOKAS_OS == _EOKAS_OS_MACOS
-
-#elif _EOKAS_OS == _EOKAS_OS_LINUX
-
-#include <unistd.h>
-#include <limits.h>
-
-namespace eokas
-{
-    String OS::getCurrentUser()
-    {
-        char buffer[LOGIN_NAME_MAX];
-        getlogin_r(buffer, LOGIN_NAME_MAX);
-        return String(buffer);
-    }
-    
-    String OS::getMacAddress() {
-        struct ifaddrs* ifap;
-        struct ifaddrs* ifa;
-        struct sockaddr_dl* sdl;
-    
-        if (getifaddrs(&ifap) != 0) {
-            return "";
-        }
-    
-        String macAddress;
-        for (ifa = ifap; ifa; ifa = ifa->ifa_next) {
-            if ((ifa->ifa_addr->sa_family == AF_LINK) && ((sdl = (struct sockaddr_dl *)ifa->ifa_addr)->sdl_type == IFT_ETHER)) {
-                char* mac = (char*)ether_ntoa((const struct ether_addr*)LLADDR(sdl));
-                macAddress = mac;
+        String macAddr;
+        PIP_ADAPTER_INFO adapterPtr = adapterInfo;
+        while (adapterPtr != nullptr) {
+            if(adapterPtr->Type == MIB_IF_TYPE_ETHERNET) {
+                macAddr += String::format("%02X", adapterPtr->Address[0]);
+                for (UINT i = 1; i < adapterPtr->AddressLength; i++) {
+                    macAddr += String::format("-%02X", adapterPtr->Address[i]);
+                }
                 break;
             }
+            adapterPtr = adapterPtr->Next;
+        }
+        return macAddr;
+#else
+        struct ifaddrs* addrs;
+        if (getifaddrs(&addrs) != 0) {
+            return "";
+        }
+        
+        String macAddress;
+        for (struct ifaddrs* addrPtr = addrs; addrPtr != nullptr; addrPtr = ifa->ifa_next) {
+            if(addrPtr->ifa_addr->sa_family == AF_LINK) {
+                struct sockaddr_dl* sdl = (struct sockaddr_dl*)addrPtr->ifa_addr;
+                if(sdl->sdl_type == IFT_ETHER) {
+                    char* mac = (char*)ether_ntoa((const struct ether_addr*)LLADDR(sdl));
+                    macAddress = mac;
+                    break;
+                }
+            }
         }
     
-        freeifaddrs(ifap);
+        freeifaddrs(addrs);
         
         return macAddress;
+#endif
     }
 }
-
-#elif _EOKAS_OS == _EOKAS_OS_IOS
-
-#elif _EOKAS_OS == _EOKAS_OS_ANDROID
-
-#endif
--- src/base/os.h
+++ src/base/os.h
@@ -6,29 +6,15 @@
 #include "./string.h"
 
 namespace eokas {
-    struct CpuState {
-        u64_t frequency = 0;
-    };
-    
-    struct MemoryState {
-        size_t total = 0;
-        size_t available = 0;
-    };
-    
     struct OS {
         static String getSystemName();
         static String getSystemVersion();
         static String getDeviceName();
         static String getDeviceModel();
-        static String getUniqueIdentifier();
         static u32_t getCpuCount();
-        static CpuState& getCpuState(u32_t index);
-        static MemoryState& getMemoryState();
         static String getEnv(const String& name);
-        static void setEnv(const String& name, const String& value);
+        static bool setEnv(const String& name, const String& value);
         static String getCurrentUser();
-        static u32_t getCurrentProcess();
-        static u32_t getCurrentThread();
         static String getMacAddress();
     };
 }
