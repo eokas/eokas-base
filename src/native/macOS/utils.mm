5922bf2c56eaa094238d9108551ea655fb87fc70
Qilin <qilin.cheng@foxmail.com>
2024-09-08 01:08:50 +0800

    [native] ADD: macOS OpenFileDialog and OpenDirectoryDialog

--- src/native/macOS/utils.mm
+++ src/native/macOS/utils.mm
@@ -13,7 +13,16 @@ namespace eokas {
             panel.prompt = @"OK";
             panel.canChooseFiles = YES;
             panel.allowsMultipleSelection = NO;
-            panel.allowedFileTypes = [NSArray arrayWithObjects:@"txt", @"cpp", @"h", nil];
+
+            panel.allowedFileTypes = @[];
+            NSMutableArray<NSString *> *allowedTypes = [NSMutableArray array];
+            for (auto const& filter : filters) {
+                NSString* fileTitle = [NSString stringWithUTF8String: filter.first.cstr()];
+                NSString* fileFilter = [NSString stringWithUTF8String: filter.second.cstr()];
+                NSString *extension = [fileFilter substringFromIndex:2];
+                [allowedTypes addObject:extension];
+            }
+            panel.allowedFileTypes = [allowedTypes copy];
             
             if ([panel runModal] == NSModalResponseOK) {
                 NSURL *fileURL = [[panel URLs] objectAtIndex:0];
@@ -30,8 +39,9 @@ namespace eokas {
             panel.message = @"Open";
             panel.prompt = @"OK";
             panel.canChooseFiles = NO;
+            panel.canChooseDirectories = YES;
             panel.allowsMultipleSelection = NO;
-            panel.allowedFileTypes = [NSArray arrayWithObjects:@"txt", @"cpp", @"h", nil];
+            panel.directoryURL = [NSURL URLWithString: [NSString stringWithUTF8String: defaultPath.cstr()]];
             
             if ([panel runModal] == NSModalResponseOK) {
                 NSURL *fileURL = [[panel URLs] objectAtIndex:0];
