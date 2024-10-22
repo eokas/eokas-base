96d4c02aeb65d968eef447c77ed23e9af121a5ed
qilincheng <>
2024-10-22 22:50:18 +0800

    [app] ADD: app framework

---
+++ src/app/app.cpp
@@ -0,0 +1,26 @@
+#include "app/app.h"
+#include "native/utils.h"
+
+namespace eokas {
+    bool App::init() {
+        String configJson;
+        if(!File::readText("../config/config.json", configJson))
+            return false;
+        std::shared_ptr<HomValue> json = JSON::parse(configJson);
+        
+        mModules.init();
+        return true;
+    }
+    
+    void App::quit() {
+        mModules.quit();
+    }
+    
+    void App::tick(float deltaTime) {
+        mModules.quit();
+    }
+    
+    ModuleManager& App::modules() {
+        return mModules;
+    }
+}
